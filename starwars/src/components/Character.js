96e593c17b16fe234401dfafc6930aa1a9c72f55
emilme <emilme@sir.mle>
2020-06-12 08:51:44 -0700

    Styling Started

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,12 +1,13 @@
 // Write your Character component here
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
+import { Card, CardBody, Button, CardTitle, CardText, CardImg } from 'reactstrap';
 
-function Character(){
+function Character(props){
     const [data,setData] = useState([])
 
     useEffect(()=>{
-        axios.get('https://rickandmortyapi.com/api/character/1,183')
+        axios.get('https://rickandmortyapi.com/api/character/1')
         .then(res=>{
             console.log(res.data)
             setData(res.data)
@@ -17,7 +18,16 @@ function Character(){
 
     return (
     <div className='character'>
-        Test
+        <Card>
+            <CardImg top width="100%" src={data.image} alt="Card image cap" />
+            <CardBody>
+                <CardTitle>{props.name}</CardTitle>
+                <CardText>{props.episode}</CardText>
+                <CardText>
+                    <small className="text-muted">{props.status}</small>
+                </CardText>
+            </CardBody>
+        </Card>
     </div>)
 }
 
