346be2b2d4e75dc45c0c4efb3f8b12c0c689ed51
emilme <emilme@sir.mle>
2020-06-12 09:10:22 -0700

    Chacters properly displaying

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,30 +1,17 @@
 // Write your Character component here
-import React,{useState,useEffect} from 'react';
-import axios from 'axios';
-import { Card, CardBody, Button, CardTitle, CardText, CardImg } from 'reactstrap';
+import React from 'react';
+import { Card, CardBody, CardTitle, CardText, CardImg } from 'reactstrap';
 
 function Character(props){
-    const [data,setData] = useState([])
-
-    useEffect(()=>{
-        axios.get('https://rickandmortyapi.com/api/character/1')
-        .then(res=>{
-            console.log(res.data)
-            setData(res.data)
-        })
-        .catch(err => {console.log('Error',err)})
-
-    },[])
-
     return (
     <div className='character'>
         <Card>
-            <CardImg top width="100%" src={data.image} alt="Card image cap" />
+            <CardImg top width="100%" src={props.character.image} alt="Card image cap" />
             <CardBody>
-                <CardTitle>{props.name}</CardTitle>
-                <CardText>{props.episode}</CardText>
+                <CardTitle>{props.character.name}</CardTitle>
+                <CardText>{props.character.species}</CardText>
                 <CardText>
-                    <small className="text-muted">{props.status}</small>
+                    <small className="text-muted">{props.character.status}</small>
                 </CardText>
             </CardBody>
         </Card>
