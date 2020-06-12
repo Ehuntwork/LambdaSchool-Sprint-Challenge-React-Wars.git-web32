88ddf2d5852133054f248d61070f5483fc3273e9
emilme <emilme@sir.mle>
2020-06-12 08:42:31 -0700

    api data succesfully retrived

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+function Character(){
+    const [data,setData] = useState([])
+
+    useEffect(()=>{
+        axios.get('https://rickandmortyapi.com/api/character/1,183')
+        .then(res=>{
+            console.log(res.data)
+            setData(res.data)
+        })
+        .catch(err => {console.log('Error',err)})
+
+    },[])
+
+    return (
+    <div className='character'>
+        Test
+    </div>)
+}
+
+export default Character;
